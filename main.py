95e40c2ae77ca2e1586154d5cdc428549df2b308
Natanael Jop <natanael.jop@icloud.com>
2024-05-17 18:16:29 +0200

    Task2: wczytywanie do obiektu z pliku .json i weryfikacja poprawności składni pliku

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def parse_arguments():
     if len(sys.argv) != 3:
@@ -7,3 +8,12 @@ def parse_arguments():
     input_file = sys.argv[1]
     output_file = sys.argv[2]
     return input_file, output_file
+
+def load_json(input_file):
+    try:
+        with open(input_file, 'r') as file:
+            data = json.load(file)
+        return data
+    except Exception as e:
+        print(f"Nie można wczytać pliku JSON: {e}")
+        sys.exit(1)
